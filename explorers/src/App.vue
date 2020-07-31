298f35e73e657be3e4bb777cd750cfb0e8e7c98b
rhendrik <rhendrik@student.wethinkcode.co.za>
2020-07-31 21:10:23 +0200

    working on the dashboard

--- explorers/src/App.vue
+++ explorers/src/App.vue
@@ -1,14 +1,30 @@
 <template>
-  <div id="app">
-    <h1 class="notification is-primary">FloodLight</h1>
+  <div id="app" class=" notification is-warning">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+    <div class="notification is-link hero">
+      <dv class="hero-body">
+      <i class="material-icons has-text-warning">wb_sunny</i>
+      <h1 class="notification is-link has-text-white hero-title">FloodLight</h1>
+      <p class="hero-text">shining light on women's safety</p>
+    </dv>
+    </div>
+    <dash></dash>
+
+    <div class="notification is-warning">
+      <footer class="notification is-link has-text-white">explorers</footer> 
+    </div>
   </div>
 </template>
 
 <script>
+  import Dashboard from './components/Dashboard.vue'
 
-export default {
-  name: 'App',
-}
+  export default {
+    name: 'App',
+    components: {
+      dash: Dashboard,
+    }
+  }
 </script>
 
 <style>
---
+++ explorers/src/components/Dashboard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div> 
+      <h1>search by area</h1>
+    </div>
+    <div>
+      <h1>search by safety</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default{
+    name: 'Dash',
+  }
+</script>
+
+<style></style>
